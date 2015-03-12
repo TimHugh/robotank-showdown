2b1c29a993bb0892b7781c9b5a95d581ce72b50d
Tim Heuett <tim.heuett@gmail.com>
2015-03-11 19:46:44 -0700

    add re-aiming if target is lost

--- ruffrida.rb
+++ ruffrida.rb
@@ -4,35 +4,63 @@ require 'rrobots'
 class Ruffrida
   include Robot
 
+  attr_reader :last_enemy_sighting_time
+  attr_reader :enemy_found
+
   def initialize
     @enemy_found = false
+    @last_enemy_sighting_time = 0
+    @current_direction = :clockwise
   end
 
   def tick(events)
-    unless @enemy_found
-      move_counter_clockwise
-      search_for_enemy
-    else
-      move_clockwise
+    move
+
+    @previous_energy = energy
+
+    if enemy_found
       maintain_gun_direction
       shoot
+    else
+      search_for_enemy
     end
+
+    reverse_directions if sustained_injury?
+    last_enemy_sighting_time = time if enemy_sighting
+    @enemy_found = false if time_since_last_enemy_sighting > 150
   end
 
   def enemy_sighting
     not events['robot_scanned'].empty?
   end
 
-  def move_counter_clockwise
+  def time_since_last_enemy_sighting
+    time - last_enemy_sighting_time
+  end
+
+  def move_anticlockwise
     accelerate 1
     turn 2
   end
 
+  def sustained_injury?
+    energy < @previous_energy
+  end
+
   def search_for_enemy
     if enemy_sighting
-      @enemy_found = true
+      self.enemy_found = true
     else
-      turn_gun 20
+      turn_gun 10
+    end
+  end
+
+  def move
+    case @current_direction
+    when :clockwise
+      move_clockwise
+    when :anticlockwise
+      move_anticlockwise
     end
   end
 
@@ -41,8 +69,26 @@ class Ruffrida
     turn -2
   end
 
+  def enemy_found=(val)
+    @last_enemy_sighting_time = time if val
+    @enemy_found = val
+  end
+
   def maintain_gun_direction
-    turn_gun 2
+    case @current_direction
+    when :clockwise
+      turn_gun 2
+    when :anticlockwise
+      turn_gun - 2
+    end
+  end
+
+  def reverse_directions
+    if @current_direction == :clockwise
+      @current_direction = :anticlockwise
+    else
+      @current_direction = :clockwise
+    end
   end
 
   def shoot
